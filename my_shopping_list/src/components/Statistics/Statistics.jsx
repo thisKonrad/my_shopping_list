89a0b05bd9351052811d7537da6702ab4b6437a1
this.Konrad <konradlebt@gmail.com>
2023-12-09 17:15:53 +0100

    add the messages for statistics

--- my_shopping_list/src/components/Statistics/Statistics.jsx
+++ my_shopping_list/src/components/Statistics/Statistics.jsx
@@ -4,12 +4,20 @@ import './statistics.css'
 
 export default function Statistics({items}) {
 
+    if(items.length === 0){
+        return <p className="statistics">Let's go Shopping!</p>
+    }
+
     const numberOfItems= items.length;
     const itemsPacked = items.filter((item)=> item.packed).length;
     const listPackedPercentage = Math.round(((itemsPacked/numberOfItems ) *100));
 
-    return (<footer className="stats">
-        <em>you have {numberOfItems} items on your list, 
-        and you already packed {itemsPacked} item, you packed {listPackedPercentage}% of the List</em>
+    return (<footer className="statistics">
+        <p>
+        {listPackedPercentage === 100 ? `You are Finished! Lets go home!` :
+        `you have ${numberOfItems} items on your list, 
+        and you already packed ${itemsPacked} item, 
+        you packed ${listPackedPercentage}% of the List`}
+        </p>
     </footer>)
 }
